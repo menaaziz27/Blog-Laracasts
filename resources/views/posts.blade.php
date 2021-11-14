7623e392d6bd5cecf7e9671175e868d9912024d3
menaaziz27 <menaaziz27@gmail.com>
2021-11-14 15:05:18 +0200

    cache

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+<h1><a href="/posts/my-first-post">first post</a></h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad adipisci aut commodi consequatur consequuntur cum cumque cupiditate deleniti ea eligendi eos error est eum, excepturi fuga harum impedit incidunt ipsam iusto laboriosam natus non odit perspiciatis quaerat quas quis quod rem sit suscipit tenetur veniam vitae voluptas! Aperiam, sit.</p>
+
+<h1><a href="/posts/my-second-post">second post</a></h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad adipisci aut commodi consequatur consequuntur cum cumque cupiditate deleniti ea eligendi eos error est eum, excepturi fuga harum impedit incidunt ipsam iusto laboriosam natus non odit perspiciatis quaerat quas quis quod rem sit suscipit tenetur veniam vitae voluptas! Aperiam, sit.</p>
+
+<h1><a href="/posts/my-third-post">third post</a></h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad adipisci aut commodi consequatur consequuntur cum cumque cupiditate deleniti ea eligendi eos error est eum, excepturi fuga harum impedit incidunt ipsam iusto laboriosam natus non odit perspiciatis quaerat quas quis quod rem sit suscipit tenetur veniam vitae voluptas! Aperiam, sit.</p>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}', function ($slug) {
+    $post = \App\Models\Post::find($slug);
+
+    return view("post", [
+        "post" => $post
+    ]);
+})->where('post', '[A-z_\-]+');
