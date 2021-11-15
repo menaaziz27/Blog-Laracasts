18bfd1731c952de946dc97fa3acbaa286235b0e1
menaaziz27 <menaaziz27@gmail.com>
2021-11-16 00:26:54 +0200

    blade templates

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,12 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-<body>
-<?php foreach($posts as $post) : ?>
-<article>
-    <h1><a href="/posts/{{$post->slug}}">{{ $post->title  }}</a></h1>
-    <div>
-        <?= $post->body ?>
-    </div>
-</article>
-<?php endforeach; ?>
-</body>
-</html>
+@extends ("layout")
+
+@section("content")
+@foreach($posts as $post)
+    <article class="{{ $loop->even ? "active" : ""  }}">
+        <h1><a href="/posts/{{$post->slug}}">{{ $post->title  }}</a></h1>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+@endforeach
+@endsection
